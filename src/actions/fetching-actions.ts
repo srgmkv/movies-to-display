7ef6438fe2552ed6f56599209856f9f0d3e9694f
srgmkv <ms5533@yandex.ru>
2019-08-08 19:54:02 +0700

    backupurl

--- src/actions/fetching-actions.ts
+++ src/actions/fetching-actions.ts
@@ -21,11 +21,16 @@ export function dataRequested(): IActionDataRequested {
   return { type: 'DATA_REQUESTED' }
 }
 
-export function getMoviesData():any {
+export function getMoviesData(): any {
   return (dispatch: any) => {
     dispatch(dataRequested())
 
-    axios("http://s3-eu-west-1.amazonaws.com/sequeniatesttask/films.json")
+    const url = "https://s3-eu-west-1.amazonaws.com/sequeniatesttask/films.json"
+    const backupUrl = "https://raw.githubusercontent.com/srgmkv/movies-to-display/master/public/films-backup.json"
+//запрашиваем данные с  url,если ошибка, то с backupUrl, если ошибка,то обрабатываем ее/записываем,
+    axios(url)
+      .then((payload: any) => dispatch({ type: 'DATA_LOADED', payload }))
+      .catch(() => axios(backupUrl))
       .then((payload: any) => dispatch({ type: 'DATA_LOADED', payload }))
       .catch((err: any) => dispatch({ type: 'SERVER_ERRORED', errorData: err }))
   }
